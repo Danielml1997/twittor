9db0c9f333b4c2a5a5a5415e20fa32b3af8c0660
Daniel Villavicencio <daniel.istpa@gmail.com>
2021-11-15 00:53:08 -0500

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,19 @@
+
+
+function actualizaCacheDinamico( dynamicCache, req, res){
+
+    if( res.ok){
+
+        return caches.open(DYNAMIC_CACHE).then( cache =>{
+
+            cache.put( req, res.clone() );
+            return res.clone();
+
+        });
+    } else {
+
+        return res;
+
+    }
+    
+}
